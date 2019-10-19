ec03f6fd548b9a9c4ee840fa3995973b18dde181
zunsthy <zunsthy@gmail.com>
2019-10-19 17:14:52 +0800

    new member message: add full name

--- server/services/message/newMembers.js
+++ server/services/message/newMembers.js
@@ -4,7 +4,6 @@ module.exports = (data, cb) => {
   const {
     message_id,
     chat,
-    new_chat_participant,
     new_chat_members,
   } = data;
 
@@ -14,15 +13,15 @@ module.exports = (data, cb) => {
   }
 
   new_chat_members.forEach((user) => {
-    const name = user.first_name.slice(0, 100);
+    const firstName = user.first_name.slice(0, 32);
+    const lastName = user.last_name ? user.last_name.slice(0, 32) : '';
 
     callapi('sendMessage', {
       chat_id: chat.id,
       reply_to_message_id: message_id,
       disable_web_page_preview: true,
       disable_notification: true,
-      text: '\u6B22\u8FCE ' + name + ' \u5165\u7FA4',
+      text: `欢迎 ${firstName}${lastName} 加入群聊`,
     }, cb);
   });
 };
-
